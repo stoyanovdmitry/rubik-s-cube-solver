1cb930c79b41d33f82b1ef1f7195ab5e8a688ea6
stoyanovdmitry <stoyanov.dmt@gmail.com>
2017-10-31 11:36:12 +0200

    added empty PhaseSix

---
+++ src/main/java/stoyanovdmitry/solver/PhaseSix.java
@@ -0,0 +1,20 @@
+package stoyanovdmitry.solver;
+
+import stoyanovdmitry.cube.Cube;
+
+public class PhaseSix extends AbstractPhase {
+
+	public PhaseSix(Cube cube) {
+		super(cube);
+	}
+
+	@Override
+	public void computePhase() {
+
+	}
+
+	@Override
+	public boolean isPhaseDone() {
+		return false;
+	}
+}
