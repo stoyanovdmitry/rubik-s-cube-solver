be3cad6d940cedecb8bae7c593eb1249a71cb04e
stoyanovdmitry <stoyanov.dmt@gmail.com>
2017-11-06 20:19:07 +0200

    Solver completed

--- src/main/java/stoyanovdmitry/solver/Solver.java
+++ src/main/java/stoyanovdmitry/solver/Solver.java
@@ -2,6 +2,7 @@ package stoyanovdmitry.solver;
 
 import stoyanovdmitry.cube.Cube;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Solver {
@@ -13,16 +14,38 @@ public class Solver {
 
 	public Solver(Cube cube) {
 		this.cube = cube;
+
+		phases = new ArrayList<>();
+
+		phases.add(new PhaseOne());
+		phases.add(new PhaseTwo());
+		phases.add(new PhaseThree());
+		phases.add(new PhaseFour());
+		phases.add(new PhaseFive());
+		phases.add(new PhaseSix());
+		phases.add(new PhaseSeven());
+		phases.add(new PhaseEight());
 	}
 
 	public String getSolve() {
-
-		if(solve == null) solveCube();
+		if (solve == null) solveCube();
 		return solve;
+	}
+
+	public Cube getCube() {
+		return cube;
 	}
 
 	private void solveCube() {
 
-		//todo realize method
+		solve = "";
+
+		for (Phase phase : phases) {
+
+			phase.setCube(cube);
+			phase.computePhase();
+			solve += phase.getPhaseSolve();
+			this.cube = phase.getCube();
+		}
 	}
 }
