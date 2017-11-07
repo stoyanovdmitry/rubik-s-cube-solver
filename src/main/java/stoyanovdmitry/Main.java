bf713bbc071a6214c2c2f4cb7fb20f8a84310a0f
stoyanovdmitry <stoyanov.dmt@gmail.com>
2017-11-07 12:59:58 +0200

    started creating a GUI

--- src/main/java/stoyanovdmitry/Main.java
+++ src/main/java/stoyanovdmitry/Main.java
@@ -1,7 +1,15 @@
 package stoyanovdmitry;
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
 
 	public static void main(String[] args) {
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
 	}
 }
